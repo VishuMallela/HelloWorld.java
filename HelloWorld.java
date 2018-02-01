8420fb0ab228a563c795387a9768af997491929d
Vishu Mallela <vishumallela@icloud.com>
2018-02-01 21:29:50 +0530

    Added code
    Added the code from mac

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/* Program to print the text Hello World! on the output screen using JAVA */
+
+public class HelloWorld // File name is HelloWorld.java
+{
+	public static void main (String[] args) { // Code begins executing from here
+		System.out.println ("Hello World! \n"); // Text is printed on the output screeen because of this line of code
+	}
+
+}
